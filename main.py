5575839459738f91abe6a8526f9c0460577f4f5c
Kacper Kujawa <kacper.kujawa98@gmail.com>
2023-05-18 20:46:18 +0200

    Fix evaluate function

--- main.py
+++ main.py
@@ -12,12 +12,16 @@ def load_iris():
     return features, classes
 
 
-# def evaluate(clusters, labels):
-#     for cluster in np.unique(clusters):
-#         labels_in_cluster = labels[clusters == cluster]
-#         print(f"Cluster: {cluster}")
-#         for label_type in np.unique(labels):
-#             print(f"Num of {label_type}: {np.sum(labels_in_cluster == label_type)}")
+def evaluate(clusters, labels):
+    unique_clusters = np.unique(clusters, axis=0)
+    for cluster in unique_clusters:
+        labels_in_cluster = np.array([])
+        for i in range(len(clusters)):
+            if np.equal(clusters[i], cluster).all():
+                labels_in_cluster = np.append(labels_in_cluster, labels[i])
+        print(f"Cluster: {cluster}")
+        for label_type in np.unique(labels):
+            print(f"Num of {label_type}: {np.sum(labels_in_cluster == label_type)}")
 
 
 def clustering(kmeans_pp):
@@ -26,11 +30,11 @@ def clustering(kmeans_pp):
     intra_class_variance = []
     for i in range(100):
         assignments, centroids, error = k_means(features, 3, kmeans_pp)
-        # evaluate(assignments, classes)
+        evaluate(assignments, classes)
         intra_class_variance.append(error)
     print(f"Mean intra-class variance: {np.mean(intra_class_variance)}")
 
 
 if __name__ == "__main__":
-    clustering(kmeans_pp=False)
-    # clustering(kmeans_pp=True)
+    # clustering(kmeans_pp=False)
+    clustering(kmeans_pp=True)
